7ee307c57a049062e7884d6786e057e56546efa4
Filip Gerhat <filip.gerhat.1@gmail.com>
2021-07-30 21:27:29 +0200

    finished simple logic for setting up WiFi driver and connecting to wifi

--- main/main.c
+++ main/main.c
@@ -1,34 +1,99 @@
-/* Hello World Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
 
+#define WIFI_SSID                   CONFIG_WIFI_SSID
+#define WIFI_PASSWORD               CONFIG_WIFI_PASSWORD
+#define WIFI_MAX_RETRY_ATTEMPTS     CONFIG_WIFI_MAX_RETRY_ATTEMPTS
 
-void app_main(void)
+static int wifi_retries = 0;
+
+void on_wifi_event(void* arg, esp_event_base_t base, int32_t id, void* event_data)
 {
+    switch(id)
+    {
+        case WIFI_EVENT_STA_START:
+            printf("wiFi connecting...\n");
+            esp_wifi_connect();
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            if(wifi_retries < WIFI_MAX_RETRY_ATTEMPTS)
+            {
+                printf("WiFi connection failed, retrying (attempt %d)...\n", ++wifi_retries);
+                esp_wifi_connect();
+            }
+            //TODO improve, maybe use event groups or semaphores
+            else
+            {
+                printf("Could not connect to WiFi!\n");
+                fflush(stdout);
+                esp_restart();
+            }
+            break;
+    }
+}
+
+void on_ip_event(void* arg, esp_event_base_t base, int32_t id, void* event_data)
+{
+    switch(id)
+    {
+        case IP_EVENT_STA_GOT_IP:
+            wifi_retries = 0;
+            printf("WiFi Connected!\n");
+            break;
+    }
+}
+
+void app_main()
+{
+    //initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    //initialize NETIF
+    ESP_ERROR_CHECK(esp_netif_init());
+    //create default event loop
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    //create default wifi station
+    esp_netif_create_default_wifi_sta();
+    //initialize default WiFi driver
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    //wifi configuration
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    wifi_config_t conf = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD   
+        }
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &conf));
+
+    //register wifi and ip event handlers
+    esp_event_handler_instance_t instance_wifi_handler;
+    esp_event_handler_instance_t instance_ip_handler;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &on_wifi_event, NULL, &instance_wifi_handler));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &on_ip_event, NULL, &instance_ip_handler));
+
+    //start wifi driver
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+
+
     printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    printf("Restarting in 1 minute\n");
+    vTaskDelay(50000 / portTICK_PERIOD_MS);
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
