43e5d8d7a0ab813a0fe7ac45bb0736542bdf48b5
Filip Gerhat <filip.gerhat.1@gmail.com>
2021-07-31 19:58:58 +0200

    simple mqtt message publishing with no disconnect handling

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "esp_wifi.h"
+#include "mqtt_client.h"
 
 #define WIFI_SSID                   CONFIG_WIFI_SSID
 #define WIFI_PASSWORD               CONFIG_WIFI_PASSWORD
@@ -16,12 +17,22 @@
 #define WIFI_CONNECTED_BIT          BIT0
 #define WIFI_FAIL_BIT               BIT1
 
-static const char* TAG_WIFI = "WiFi Module";
-static const char* TAG_APP = "Main Application Module";
+#define MQTT_BROKER_URL             CONFIG_MQTT_BROKER_URL
+#define MQTT_USERNAME               CONFIG_MQTT_USERNAME
+#define MQTT_PASSWORD               CONFIG_MQTT_PASSWORD
+#define MQTT_TOPIC                  CONFIG_MQTT_TOPIC
+
+#define MQTT_CONNECTED_BIT          BIT0
 
 static int wifi_retries = 0;
+static int mqtt_message_number = 0;
 
 static EventGroupHandle_t event_group_wifi;
+static EventGroupHandle_t event_group_mqtt;
+
+static const char* TAG_WIFI = "WiFi Module";
+static const char* TAG_APP = "Main Application Module";
+static const char* TAG_MQTT = "MQTT Module";
 
 void on_wifi_event(void* arg, esp_event_base_t base, int32_t id, void* event_data)
 {
@@ -54,6 +65,17 @@ void on_ip_event(void* arg, esp_event_base_t base, int32_t id, void* event_data)
             wifi_retries = 0;
             ESP_LOGI(TAG_WIFI, "Connected");
             xEventGroupSetBits(event_group_wifi, WIFI_CONNECTED_BIT);
+            break;
+    }
+}
+
+void on_mqtt_event(void* arg, esp_event_base_t base, int32_t id, void* event_data)
+{
+    switch(id)
+    {
+        case MQTT_EVENT_CONNECTED :
+            ESP_LOGI(TAG_MQTT, "Connected to broker");
+            xEventGroupSetBits(event_group_mqtt, MQTT_CONNECTED_BIT);
             break;
     }
 }
@@ -127,22 +149,87 @@ void app_main()
     if(event_bits_wifi & WIFI_FAIL_BIT)
     {
         ESP_LOGE(TAG_APP, "No WiFi connection");
+
+        for (int i = 10; i >= 0; i--) {
+            printf("Restarting in %d seconds...\n", i);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        printf("Restarting now.\n");
+        fflush(stdout);
+        esp_restart();
     }
     else if(event_bits_wifi & WIFI_CONNECTED_BIT)
     {
         ESP_LOGI(TAG_APP, "Connected to WiFi");
     }
 
-    printf("Hello world!\n");
-
-    printf("Restarting in 1 minute\n");
-    vTaskDelay(50000 / portTICK_PERIOD_MS);
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    //create event group MQTT
+    event_group_mqtt = xEventGroupCreate();
+    if(event_group_mqtt == NULL)
+    {
+        ESP_LOGE(TAG_APP, "Error creating MQTT event group");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        abort();
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+
+    //configure MQTT client
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = MQTT_BROKER_URL,
+        .username = MQTT_USERNAME,
+        .password = MQTT_PASSWORD
+    };
+
+    //initialize mqtt client
+    esp_mqtt_client_handle_t mqtt_handle = esp_mqtt_client_init(&mqtt_cfg);
+    if(mqtt_handle == NULL)
+    {
+        ESP_LOGE(TAG_MQTT, "Error initializing MQTT client");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        abort();
+    }
+    ESP_LOGI(TAG_MQTT, "Client initialized");
+
+    //register MQTT event handler
+    ESP_ERROR_CHECK(esp_mqtt_client_register_event(mqtt_handle, ESP_EVENT_ANY_ID, on_mqtt_event, NULL));
+
+    //start MQTT client
+    ESP_ERROR_CHECK(esp_mqtt_client_start(mqtt_handle));
+    ESP_LOGI(TAG_MQTT, "Client started");
+
+    //wait for MQTT connection to broker
+    xEventGroupWaitBits(
+        event_group_mqtt,
+        MQTT_CONNECTED_BIT,
+        pdTRUE,
+        pdFALSE,
+        portMAX_DELAY);
+
+    while(1)
+    {
+        char buffer[32];
+        int length = sprintf(buffer, "Message %d", mqtt_message_number++);
+
+        if(length <= 0)
+        {
+            ESP_LOGE(TAG_APP, "Error creating MQTT message payload");
+            vTaskDelay(5000 / portTICK_PERIOD_MS);
+            abort();
+        }
+        ++length;
+
+        ESP_LOGI(TAG_APP, "Sending MQTT message \"%s\"", buffer);
+
+        esp_mqtt_client_publish(
+            mqtt_handle,
+            MQTT_TOPIC,
+            buffer,
+            length,
+            1,
+            0
+        );
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    }
+
+    printf("Hello world!\n");
 }
